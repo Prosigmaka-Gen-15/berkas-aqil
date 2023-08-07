3c77467149fdcf5beb86bf6d21d05274e08166b7
MuhammadAqil25 <muhammadaqil2529@gmail.com>
2023-08-07 23:20:56 +0700

    menambahkan pop up saat delete

--- gen-23.05-react-crud/src/component/ListProduk.jsx
+++ gen-23.05-react-crud/src/component/ListProduk.jsx
@@ -17,6 +17,7 @@ const schema = yup.object().shape({
 });
 
 export default function ListProduk() {
+    const [showModal, setShowModal] = React.useState(false);
     const [laptops, setLaptops] = useState([]);
     const navigate = useNavigate([]);
 
@@ -38,7 +39,7 @@ export default function ListProduk() {
     };
 
     const deleteLaptop = (laptopId) => {
-        console.log(laptopId);
+        alert('Data Berhasil Dihapus');
         axios
             .delete('http://localhost:3000/laptop/' + laptopId)
             .then(() => getLaptops())
@@ -110,11 +111,71 @@ export default function ListProduk() {
                                                 <Button
                                                     className='flex flex-row gap-1 items-center'
                                                     onClick={() =>
-                                                        deleteLaptop(laptop.id)
+                                                        setShowModal(true)
                                                     }
                                                 >
                                                     <MdDelete /> Delete
                                                 </Button>
+                                                {showModal ? (
+                                                    <>
+                                                        <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+                                                            <div className='relative w-96 my-6 mx-auto max-w-3xl'>
+                                                                {/*content*/}
+                                                                <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+                                                                    {/*header*/}
+                                                                    <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
+                                                                        <h3 className='text-2xl font-semibold'>
+                                                                            Apakah
+                                                                            Kamu
+                                                                            Yakin
+                                                                            Ingin
+                                                                            Menghapus
+                                                                            Data
+                                                                            Ini?
+                                                                        </h3>
+                                                                        <button
+                                                                            className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
+                                                                            onClick={() =>
+                                                                                setShowModal(
+                                                                                    false,
+                                                                                )
+                                                                            }
+                                                                        >
+                                                                            <span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
+                                                                                ×
+                                                                            </span>
+                                                                        </button>
+                                                                    </div>
+                                                                    <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
+                                                                        <Button
+                                                                            className='text-white bg-red-500 hover:bg-red-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                                                                            type='button'
+                                                                            onClick={() =>
+                                                                                setShowModal(
+                                                                                    false,
+                                                                                )
+                                                                            }
+                                                                        >
+                                                                            No
+                                                                        </Button>
+                                                                        <Button
+                                                                            className='bg-emerald-500 text-white hover:bg-emerald-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                                                                            type='button'
+                                                                            onClick={() =>
+                                                                                deleteLaptop(
+                                                                                    laptop.id,
+                                                                                )
+                                                                            }
+                                                                        >
+                                                                            Yes
+                                                                        </Button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+                                                    </>
+                                                ) : null}
                                             </div>
                                         </td>
                                     </tr>
@@ -152,8 +213,9 @@ function Modal() {
                             <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
                                 {/*header*/}
                                 <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t'>
-                                    <h3 className='text-3xl font-semibold'>
-                                        Add Product
+                                    <h3 className='text-2xl font-semibold'>
+                                        Apakah Kamu Yakin Ingin Menghapus Data
+                                        Ini?
                                     </h3>
                                     <button
                                         className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
@@ -164,81 +226,20 @@ function Modal() {
                                         </span>
                                     </button>
                                 </div>
-                                {/*body*/}
-                                <div className='relative p-6 flex-auto'>
-                                    <div className='mb-6'>
-                                        <label className='block' htmlFor='nama'>
-                                            Nama Laptop
-                                        </label>
-                                        <input
-                                            type='text'
-                                            className=' border-solid border-2 rounded'
-                                            name='nama'
-                                            {...register('nama')}
-                                        ></input>
-                                        <p>{errors.nama?.message}</p>
-                                    </div>
-                                    <div className='mb-6'>
-                                        <label
-                                            className='block'
-                                            htmlFor='harga'
-                                        >
-                                            harga
-                                        </label>
-                                        <input
-                                            type='text'
-                                            className=' border-solid border-2 rounded'
-                                            name='harga'
-                                            {...register('harga')}
-                                        ></input>
-                                        <p>{errors.harga?.message}</p>
-                                    </div>
-                                    <div className='mb-6'>
-                                        <label
-                                            className='block'
-                                            htmlFor='gambar'
-                                        >
-                                            Gambar
-                                        </label>
-                                        <input
-                                            type='text'
-                                            className=' border-solid border-2 rounded'
-                                            name='gambar'
-                                            {...register('gambar')}
-                                        ></input>
-                                        <p>{errors.gambar?.message}</p>
-                                    </div>
-                                    <div className='mb-6'>
-                                        <label
-                                            className='block'
-                                            htmlFor='deskripsi'
-                                        >
-                                            Deskripsi
-                                        </label>
-                                        <input
-                                            type='text'
-                                            className=' border-solid border-2 rounded'
-                                            name='deskripsi'
-                                            {...register('deskripsi')}
-                                        ></input>
-                                        <p>{errors.deskripsi?.message}</p>
-                                    </div>
-                                </div>
-                                {/*footer*/}
                                 <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
                                     <button
                                         className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                                         type='button'
                                         onClick={() => setShowModal(false)}
                                     >
-                                        Close
+                                        No
                                     </button>
                                     <button
                                         className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                                         type='button'
                                         onClick={handleSubmit(submitForm)}
                                     >
-                                        Save Changes
+                                        Yes
                                     </button>
                                 </div>
                             </div>
